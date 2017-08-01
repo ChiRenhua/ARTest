1f97dba95eaf0db1763ef7ba0f88658d3acf42a5
Martin <crh944015688@gmail.com>
2017-08-01 23:07:45 +0800

    Initial Commit

---
+++ ARTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ARTest
+//
+//  Created by 迟人华 on 2017/8/1.
+//  Copyright © 2017年 迟人华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ARTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ARTest
+//
+//  Created by 迟人华 on 2017/8/1.
+//  Copyright © 2017年 迟人华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+#import <ARKit/ARKit.h>
+
+@interface ViewController : UIViewController
+
+@end
